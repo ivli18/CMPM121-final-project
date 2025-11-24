f3d39b36bd7ddb7a939b61828b0216b2efae359f
ivli18 <161982162+ivli18@users.noreply.github.com>
2025-11-23 20:01:39 -0800

    Change environment variable access from Deno to Node.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-/* global Deno */
 // https://vitejs.dev/config/
 export default {
-  base: Deno.env.get("REPO_NAME") || "/project",
+  base: process.env.REPO_NAME || '/project',
   server: {
     port: 3000,
     open: true,
